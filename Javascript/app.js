a18c24906bc236ab6e52f136919ac53e0cd4dc09
Richard Wang <richard2000wang@outlook.com>
2021-04-12 21:33:38 -0700

    Add Missing CSS

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,7 +1,14 @@
 const expensesElem = document.querySelector(".expenses-content");
 const expenseNameInput = document.getElementById("expenseName");
 const expensePriceInput = document.getElementById("expensePrice");
+const modelButton = document.getElementById("modelButton");
+const expenseButton = document.getElementById("expenseButton");
+const budgetInput = document.getElementById("budget");
+const budgetButton = document.getElementById("budgetButton");
 
+modelButton.addEventListener("click", function () {
+    hideModel();
+});
 
 //erase values
 function reset(){
@@ -24,3 +31,30 @@ function renderOneExpense(expenseName, expensePrice) {
     expensePriceElem.innerText = expensePrice + "$";
     expensesElem.appendChild(expensePriceElem);
 }
+
+function showModel() {
+    document.querySelector(".model").classList.remove("hidden");
+  }
+  function hideModel() {
+    document.querySelector(".model").classList.add("hidden");
+  }
+
+function updateTotalBudget() {
+  document.querySelector(".total-budget .display").innerText =
+    totalBudget + "$";
+}
+
+function updateCurrentBudget() {
+    document.querySelector(".current-budget .display").innerText =
+      currentBudget + "$";
+}
+
+function renderExpenses() {
+  expenses.forEach((expense) => {
+    renderOneExpense(expense.expenseName, expense.expensePrice);
+  });
+  updateTotalBudget();
+  updateCurrentBudget();
+}
+
+
